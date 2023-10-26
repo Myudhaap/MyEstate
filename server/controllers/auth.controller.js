7d8d87c9f7accd20a4a8cd86c9e76eb02beb627e
Muhammad Yudha Adi Pratama <muhyudhapratama55@gmail.com>
2023-10-26 10:49:18 +0700

    create signup api route

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+import User from "../models/User.model.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, email, password: hashPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User Created Successfully..." });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 mongoose
   .connect(process.env.MONGOOURI)
